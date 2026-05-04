697def1d5bd2d41ea7466705e912a431be07fdb8
AMIT KUMAR SINGH <amitkumarsingh916631@gmail.com>
2026-05-04 22:03:56 +0530

    correct the cookie settings

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -12,7 +12,7 @@ export async function googleAuth(req,res){
         res.cookie("token",token,{
             httpOnly:true,
             secure:true,
-            sameSite: "none",
+            sameSite: "None",
             maxAge:7*24 *60*60*1000
         })
         return res.status(200).json(user);
